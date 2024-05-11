b89ed6350050f8b598300f7928a2c3aa0ed00745
chvaldez10 <christianvaldez021@gmail.com>
2024-05-11 05:25:39 -0600

    deleted archived folder

--- archive/ComplexNavbar from Materials Tailwind/ComplexNavbar/AuthButton.tsx
+++
@@ -1,9 +0,0 @@
-import { Button } from "@material-tailwind/react";
-
-export const AuthButton = () => (
-  <>
-    <Button size="sm" variant="text">
-      <span>Log In</span>
-    </Button>
-  </>
-);
