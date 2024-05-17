8e5ee107b50426fa64ee06b4d7136b52021a2c92
chvaldez10 <christianvaldez021@gmail.com>
2024-05-16 21:36:52 -0600

    fixed z index bug

--- frontend/src/components/core/hero/TextOnImage.tsx
+++ frontend/src/components/core/hero/TextOnImage.tsx
@@ -3,15 +3,19 @@ import Image from "next/image";
 const TextOnImage: React.FC<{}> = () => {
   return (
     <div className="relative flex flex-col items-center mx-auto lg:flex-row-reverse lg:max-w-5xl lg:mt-12 xl:max-w-6xl">
-      <div className="w-full h-64 lg:w-1/2 lg:h-auto">
+      {/* Image Column */}
+      <div className="relative w-full h-64 lg:w-1/2 lg:h-auto z-10">
         <img
           className="h-full w-full object-cover"
           src="https://picsum.photos/id/1018/2000"
           alt="Winding mountain road"
         />
       </div>
-      <div className="max-w-lg bg-white md:max-w-2xl md:z-10 md:shadow-lg md:absolute md:top-0 md:mt-48 lg:w-3/5 lg:left-0 lg:mt-20 lg:ml-20 xl:mt-24 xl:ml-12">
-        <div className="flex flex-col p-12 md:px-16">
+
+      {/* Text Column */}
+      <div className="relative max-w-lg bg-white md:max-w-2xl md:shadow-lg md:absolute md:top-0 md:mt-48 lg:w-3/5 lg:left-0 lg:mt-20 lg:ml-20 xl:mt-24 xl:ml-12 z-50">
+        {/* Text Wrapper */}
+        <div className="flex flex-col p-12 md:px-16 bg-white-10">
           <h2 className="text-2xl font-medium uppercase text-green-800 lg:text-4xl">
             Winding Mountain Road
           </h2>
@@ -21,6 +25,7 @@ const TextOnImage: React.FC<{}> = () => {
             ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
             aliquip ex ea commodo consequat.
           </p>
+          {/* Button Container */}
           <div className="mt-8">
             <a
               href="#"
@@ -30,7 +35,9 @@ const TextOnImage: React.FC<{}> = () => {
             </a>
           </div>
         </div>
+        {/* Close Text Wrapper */}
       </div>
+      {/* Close Text Column */}
     </div>
   );
 };
