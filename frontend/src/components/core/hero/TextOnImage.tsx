987aa02f94531b56b087591072277f87990ca1b1
chvaldez10 <christianvaldez021@gmail.com>
2024-05-19 10:20:57 -0600

    cleaned up hero section

--- frontend/src/components/core/hero/TextOnImage.tsx
+++ frontend/src/components/core/hero/TextOnImage.tsx
@@ -1,11 +1,13 @@
+"use client";
+
 import Image from "next/image";
-import { FaRegSmile, FaLeaf, FaSeedling } from "react-icons/fa";
+import PurplePillButton from "@/components/core/buttons/PurplePillButton";
 
 const TextOnImage: React.FC<{}> = () => {
   return (
-    <div className="relative flex flex-col items-center mx-auto lg:flex-row-reverse lg:max-w-5xl lg:mt-12 xl:max-w-7xl align-center">
+    <div className="relative flex flex-col items-center mx-auto lg:flex-row-reverse lg:max-w-5xl xl:max-w-6xl lg:mt-12">
       {/* Image Column */}
-      <div className="hidden lg:block w-full h-64 lg:w-576 lg:h-576 lg:ml-6">
+      <div className="hidden lg:block lg:w-576 lg:h-576 w-full h-full">
         <Image
           src="/background/Bible_with_leaf_resized.jpg"
           alt="tmp image"
@@ -17,27 +19,44 @@ const TextOnImage: React.FC<{}> = () => {
       </div>
 
       {/* Text Column */}
-      <div className="relative max-w-lg md:max-w-2xl md:shadow-lg md:absolute lg:w-3/5 lg:left-0 lg:ml-20 xl:ml-12 z-10 bg-white-0">
-        <div className="flex flex-col p-12 md:px-16 my-6">
+      <div className="relative max-w-lg md:max-w-2xl md:absolute lg:w-3/5 lg:left-0 lg:ml-10 xl:ml-12 z-10 bg-white-0 lg:shadow-lg lg:rounded-lg">
+        <div className="flex flex-col p-4 md:p-6 lg:p-12">
+          {/* Header */}
           <h2 className="text-2xl font-bold lg:text-4xl text-center">
             Welcome to Our Church
           </h2>
+
+          {/* Mission Statement */}
           <div className="mt-4 text-xl">
             <p className="font-semibold">Mission Statement</p>
             <p>
-              To <span className="font-semibold"> share</span> the good news of
+              To <span className="font-semibold">share</span> the good news of
               God’s love. To
               <span className="font-semibold"> lead</span> with a new life of
-              <span className=""> faith, trust, love, and obedience.</span> To
+              <span className="font-semibold">
+                {" "}
+                faith, trust, love, and obedience.
+              </span>{" "}
+              To
               <span className="font-semibold"> nurture</span> to become
               Christ-like in their lives.
             </p>
           </div>
+
+          {/* Service Time */}
           <div className="mt-4 text-xl">
             <p className="font-semibold">
               Sundays 2:00 PM | 419 Northmount Drive NW, Calgary, AB
             </p>
           </div>
+
+          {/* Button */}
+          <PurplePillButton
+            text="Join Us Sunday"
+            onClick={() => console.log("Join Us Sunday Clicked")}
+            variant="filled"
+            className="md:hidden mt-4"
+          />
         </div>
       </div>
     </div>
