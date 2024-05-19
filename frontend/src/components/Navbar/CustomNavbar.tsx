af815f73ba3a00558232547e0d68b2136f42cc64
chvaldez10 <christianvaldez021@gmail.com>
2024-05-18 20:07:56 -0600

    added sidebar styling

--- frontend/src/components/Navbar/CustomNavbar.tsx
+++ frontend/src/components/Navbar/CustomNavbar.tsx
@@ -4,6 +4,7 @@ import { FC, useState } from "react";
 import NavLogo from "./NavLogo";
 import MenuToggleButton from "./MenuToggleButton";
 import NavbarLinks from "./NavbarLinks";
+import Sidebar from "./Sidebar";
 
 const CustomNavbar: FC<{}> = () => {
   const [isMenuBarOpen, setIsMenuBarOpen] = useState<boolean>(false);
@@ -11,18 +12,21 @@ const CustomNavbar: FC<{}> = () => {
   const toggleMenu = () => setIsMenuBarOpen(!isMenuBarOpen);
 
   return (
-    <nav className="bg-violet-10 border-gray-200 m-2 rounded-xl">
-      <div className="max-w-screen-xl flex flex-wrap justify-between items-center mx-auto p-3">
-        <NavLogo />
-        <MenuToggleButton
-          isMenuBarOpen={isMenuBarOpen}
-          toggleMenu={toggleMenu}
-        />
-        <div className={`w-full md:block md:w-auto hidden`}>
-          <NavbarLinks />
+    <>
+      <nav className="bg-violet-10 border-gray-200 m-2 rounded-xl">
+        <div className="max-w-screen-xl flex flex-wrap justify-between items-center mx-auto p-3">
+          <NavLogo />
+          <MenuToggleButton
+            isMenuBarOpen={isMenuBarOpen}
+            toggleMenu={toggleMenu}
+          />
+          <div className="hidden md:block md:w-auto">
+            <NavbarLinks />
+          </div>
         </div>
-      </div>
-    </nav>
+      </nav>
+      <Sidebar isOpen={isMenuBarOpen} toggleMenu={toggleMenu} />
+    </>
   );
 };
 
--- frontend/src/components/Navbar/Sidebar.tsx
+++ frontend/src/components/Navbar/Sidebar.tsx
@@ -9,16 +9,21 @@ interface SidebarProps {
 const Sidebar: React.FC<SidebarProps> = ({ isOpen, toggleMenu }) => {
   return (
     <div
-      className={`fixed inset-0 bg-gray-800 bg-opacity-75 z-50 transition-transform transform ${
-        isOpen ? "translate-x-0" : "translate-x-full"
+      className={`fixed inset-0 bg-white-0 z-50 transition-transform transform ${
+        isOpen
+          ? "translate-x-0 transition ease-out duration-300"
+          : "translate-x-full ease-in duration-300"
       }`}
     >
-      <div className="flex justify-end p-4">
+      <div className="flex justify-start m-5">
         <button onClick={toggleMenu} className="text-white focus:outline-none">
           Close
         </button>
       </div>
-      <div className="flex flex-col items-center justify-center h-full">
+      <div
+        className="flex flex-col items-center justify-center"
+        style={{ height: "90vh" }}
+      >
         <NavbarLinks />
       </div>
     </div>
