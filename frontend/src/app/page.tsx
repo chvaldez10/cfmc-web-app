4d8bee6eb75f0b3c2f5d96cedf1651f93a9b91bd
chvaldez10 <christianvaldez021@gmail.com>
2024-05-19 11:12:29 -0600

    added react-icons

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,4 +1,18 @@
+import { ReactElement } from "react";
+import { FaHandshake } from "react-icons/fa";
 import TextOnImage from "@/components/core/hero/TextOnImage";
+
+const cardItems: {
+  label: string;
+  description: string;
+  svgIcon: ReactElement;
+}[] = [
+  {
+    label: "Connect",
+    description: "Fill out a description card",
+    svgIcon: <FaHandshake />,
+  },
+];
 
 export default function Home() {
   return <TextOnImage />;
