b6a2bb78a91b052ab3d52cd578a5503b006902aa
chvaldez10 <christianvaldez021@gmail.com>
2024-05-20 16:05:37 -0600

    sticky navbar

--- frontend/src/components/Navbar/CustomNavbar.tsx
+++ frontend/src/components/Navbar/CustomNavbar.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { FC, useState } from "react";
+import { FC, useEffect, useState } from "react";
 import NavLogo from "./NavLogo";
 import MenuToggleButton from "./MenuToggleButton";
 import NavbarLinks from "./NavbarLinks";
@@ -8,12 +8,32 @@ import Sidebar from "./Sidebar";
 
 const CustomNavbar: FC<{}> = () => {
   const [isMenuBarOpen, setIsMenuBarOpen] = useState<boolean>(false);
+  const [scrollPosition, setScrollPosition] = useState<number>(0);
 
   const toggleMenu = () => setIsMenuBarOpen(!isMenuBarOpen);
 
+  const handleScroll = () => {
+    const position = window.scrollY;
+    setScrollPosition(position);
+  };
+
+  useEffect(() => {
+    window.addEventListener("scroll", handleScroll, { passive: true });
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
   return (
     <>
-      <nav className="bg-gradient-to-r from-violet-50 via-slate-50 to-purple-50 ">
+      <nav
+        className={`${
+          scrollPosition > 50
+            ? "bg-violet-10"
+            : "bg-gradient-to-r from-violet-50 via-slate-50 to-purple-50"
+        } sticky top-0 z-50 transition-colors duration-300 `}
+      >
         <div className="max-w-screen-xl flex flex-wrap justify-between items-center mx-auto p-3">
           <NavLogo />
           <MenuToggleButton
