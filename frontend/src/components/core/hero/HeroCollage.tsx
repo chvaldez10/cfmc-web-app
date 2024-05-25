311acd6f21d24b701b21538699cb06dfe4e00bf2
chvaldez10 <christianvaldez021@gmail.com>
2024-05-25 08:50:38 -0600

    fixed broken layout for herocollage

--- frontend/src/components/core/hero/HeroCollage.tsx
+++ frontend/src/components/core/hero/HeroCollage.tsx
@@ -2,14 +2,15 @@ import { FC } from "react";
 import ImageCollageItem from "@/components/core/gallery/ImageCollageItem";
 import { featuredImages } from "./featuredItems";
 
-const HeroCollage: FC<{}> = () => {
+const HeroCollage: FC = () => {
   return (
     <div className="w-full bg-purple-50">
       <div
         className="mx-auto p-5 items-center justify-center lg:max-w-5xl xl:max-w-6xl flex flex-col-reverse md:flex-row lg:py-8 md:px-16 gap-4"
         style={{ height: "90vh" }}
       >
-        <div className="grid grid-cols-2 grid-rows-3 gap-2 md:gap-4 w-full md:w-2/3">
+        {/* Image Column */}
+        <div className="grid grid-cols-2 grid-rows-3 gap-2 md:gap-7 w-full md:w-2/3">
           {featuredImages.map((featuredImage, index) => (
             <ImageCollageItem
               key={index}
@@ -19,11 +20,13 @@ const HeroCollage: FC<{}> = () => {
             />
           ))}
         </div>
+
+        {/* Text Column */}
         <div className="w-full md:w-1/3 flex flex-col items-center text-center md:text-left">
           <h2 className="text-4xl font-bold my-4 bg-gradient-to-r from-purple-500 to-red-400 bg-clip-text text-transparent">
             Our Mission
           </h2>
-          <p className="w-full md:w-3/4 text-base p-4 bg-purple-100 text-gray-600 border-r-4 border-l-4 border-purple-500 shadow-lg rounded-md mb-4 md:mb-0">
+          <p className="w-full md:w-3/4 text-base 2xl:text-xl p-4 bg-purple-100 text-gray-600 border-r-4 border-l-4 border-purple-500 shadow-lg rounded-md mb-4 md:mb-0">
             To lead with new life of{" "}
             <span className="italic"> faith, trust, love, and obedience </span>
             to Jesus Christ.
--- frontend/src/components/core/hero/featuredItems.ts
+++ frontend/src/components/core/hero/featuredItems.ts
@@ -2,7 +2,7 @@ export const featuredImages = [
   {
     src: "/featured/header/2.jpeg",
     alt: "featured image 1",
-    containerClassName: "col-span-2 row-span-1 h-40 lg:h-48",
+    containerClassName: "col-span-2 row-span-1 h-40 lg:h-56",
   },
   {
     src: "/featured/header/1.jpeg",
@@ -12,11 +12,11 @@ export const featuredImages = [
   {
     src: "/featured/header/3.jpeg",
     alt: "featured image 3",
-    containerClassName: "col-span-1 row-span-1 h-40 lg:h-48",
+    containerClassName: "col-span-1 row-span-1 h-40 lg:h-56",
   },
   {
     src: "/featured/header/4.jpeg",
     alt: "featured image 4",
-    containerClassName: "col-span-1 row-span-1 h-40 lg:h-48",
+    containerClassName: "col-span-1 row-span-1 h-40 lg:h-56",
   },
 ];
