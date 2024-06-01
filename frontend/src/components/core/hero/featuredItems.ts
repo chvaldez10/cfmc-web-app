38e6a6c67aa21ac6026361890c781a04b0e5355a
chvaldez10 <christianvaldez021@gmail.com>
2024-06-01 08:21:36 -0600

    changed header image

--- frontend/src/components/core/hero/featuredItems.ts
+++ frontend/src/components/core/hero/featuredItems.ts
@@ -2,21 +2,21 @@ export const featuredImages = [
   {
     src: "/featured/header/2.jpg",
     alt: "featured image 1",
-    containerClassName: "col-span-2 row-span-1 h-40 md:h-56 2xl:h-68",
+    containerClassName: "col-span-2 row-span-1 h-40 md:h-56 2xl:h-72",
   },
   {
-    src: "/featured/header/20.jpeg",
+    src: "/featured/header/16.jpeg",
     alt: "featured image 2",
-    containerClassName: "col-span-1 row-span-1 h-40 md:h-56 2xl:h-68",
+    containerClassName: "col-span-1 row-span-1 h-40 md:h-56 2xl:h-72",
   },
   {
     src: "/featured/header/14.jpeg",
     alt: "featured image 3",
-    containerClassName: "col-span-1 row-span-1 h-40 md:h-56 2xl:h-68",
+    containerClassName: "col-span-1 row-span-1 h-40 md:h-56 2xl:h-72",
   },
   {
     src: "/featured/header/4.jpeg",
     alt: "featured image 4",
-    containerClassName: "col-span-2 row-span-1 h-40 md:h-56 2xl:h-68",
+    containerClassName: "col-span-2 row-span-1 h-40 md:h-56 2xl:h-72",
   },
 ];
