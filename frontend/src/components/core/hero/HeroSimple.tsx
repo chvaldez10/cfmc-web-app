5a4bf450d05f8d6af5dd3ffeb43cbac7714ba7a0
chvaldez10 <christianvaldez021@gmail.com>
2024-06-04 22:27:53 -0600

    changed hersimple to using grid

--- frontend/src/components/core/hero/HeroSimple.tsx
+++ frontend/src/components/core/hero/HeroSimple.tsx
@@ -16,28 +16,31 @@ const HeroSimple: FC<HeroSimpleProps> = ({
   header,
   verbiage,
   element,
-  layout = "flex flex-col md:flex-row",
+  layout = "grid grid-cols-1 md:grid-cols-2",
   backgroundColor = "",
 }) => {
   const [textRef, textInView] = useInView();
   const [mapRef, mapInView] = useInView();
 
   return (
-    <div className={`w-full ${backgroundColor}`}>
+    <div
+      className={`w-full justify-center ${backgroundColor}`}
+      style={{ height: "50vh" }}
+    >
       <div
-        className={`relative ${layout} items-center mx-auto lg:max-w-5xl xl:max-w-6xl align-center p-4 lg:p-8`}
+        className={`relative h-full ${layout} items-center mx-auto lg:max-w-5xl xl:max-w-6xl align-center p-4 lg:p-8`}
       >
         {/* Text Column */}
         <div
           ref={textRef}
           className={`${
             textInView ? "animate-slide-in-left" : ""
-          } flex-1 flex-col justify-center items-center text-center p-4 lg:p-6`}
+          } col-span-1 mx-auto justify-center items-center text-center p-4 lg:p-6`}
         >
-          <h2 className="text-3xl lg:text-4xl font-bold mb-4 text-gray-600">
+          <h2 className="text-3xl lg:text-4xl font-bold mb-4 text-gray-800">
             {header}
           </h2>
-          <p className="text-base lg:text-lg text-gray-600 w-3/4 md:w-full mx-auto">
+          <p className="text-base lg:text-lg text-gray-800 w-3/4 md:w-full mx-auto">
             {verbiage}
           </p>
         </div>
@@ -47,9 +50,11 @@ const HeroSimple: FC<HeroSimpleProps> = ({
           ref={mapRef}
           className={`${
             mapInView ? "animate-slide-in-right" : ""
-          } flex-1 flex justify-center p-4 lg:p-6`}
+          } col-span-1 flex justify-center items-center`}
         >
-          <div className="relative w-full max-w-lg">{element}</div>
+          <div className="relative w-full max-w-lg flex justify-center items-center">
+            {element}
+          </div>
         </div>
       </div>
     </div>
