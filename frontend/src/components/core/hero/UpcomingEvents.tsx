1bb222ff0008790ad76f2472c5a03e857feaed64
Christian Valdez <christianvaldez021@gmail.com>
2024-06-11 19:40:18 -0600

    removed grid styling

--- frontend/src/components/core/hero/UpcomingEvents.tsx
+++ frontend/src/components/core/hero/UpcomingEvents.tsx
@@ -7,30 +7,27 @@ const UpcomingEvents: FC = () => {
     <div className="mx-auto w-full max-w-screen-xl px-4 py-8 md:py-16 lg:py-24 lg:px-6">
       {/* Header */}
       <div className="text-center mb-8 md:mb-16">
-        <h2 className="text-4xl font-extrabold">Upcoming Events</h2>
+        <h2 className="text-4xl font-extrabold underline">Upcoming Events</h2>
       </div>
 
       {/* Time slots */}
-      <div className="divide-y divide-purple-600">
+      <div className="divide-y divide-purple-300">
         {upcomingEvents.map((upcomingEvent, index) => (
-          <div
-            key={index}
-            className="flex flex-col md:grid md:grid-rows-1 md:grid-cols-3 py-4 "
-          >
+          <div key={index} className="flex flex-col md:flex-row py-4 md:py-6">
             {/* Date */}
-            <p className="text-lg font-bold col-span-1 mb-2 md:mb-0 text-center">
-              {upcomingEvent.time}
-            </p>
+            <div className="md:w-1/3 text-center md:text-left mb-2 md:mb-0">
+              <p className="text-lg font-bold ">{upcomingEvent.time}</p>
+            </div>
 
             {/* Event Header */}
-            <p className="flex flex-col md:flex-row text-base md:text-lg truncate col-span-2 text-center md:text-left">
+            <div className="md:w-2/3 text-center md:text-left">
               <Link
                 href={upcomingEvent.link}
-                className="hover:text-purple-500 block truncate"
+                className="text-base md:text-lg  hover:text-purple-500 block truncate"
               >
                 {upcomingEvent.title}
               </Link>
-            </p>
+            </div>
           </div>
         ))}
       </div>
