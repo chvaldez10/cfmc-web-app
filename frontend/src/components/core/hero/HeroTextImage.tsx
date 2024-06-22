c455201f5b74cb1cc4df94c7c53a1309a2066245
chvaldez10 <christianvaldez021@gmail.com>
2024-06-22 14:22:21 -0600

    changed textimage size for consistancy

--- frontend/src/components/core/hero/HeroTextImage.tsx
+++ frontend/src/components/core/hero/HeroTextImage.tsx
@@ -19,13 +19,13 @@ const HeroTextImage: FC<HeroTextImageProps> = ({
     <div className="w-full min-h-screen bg-white-10 flex items-center justify-center p-4">
       <div className="flex flex-col md:flex-row mx-auto lg:w-10/12 gap-8">
         {/* Image Column */}
-        <div className="relative w-full md:w-1/2 h-60 md:h-96 order-2 md:order-1 ">
+        <div className="relative w-full md:w-1/2 h-96 md:h-576 order-2 md:order-1 ">
           <Image
             src="https://images.unsplash.com/photo-1536063766742-b514ee70707f?q=80&w=987&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
             alt="HeroTextImage Images"
             fill={true}
             style={{ objectFit: "cover" }}
-            className="rounded-none md:rounded-lg object-center"
+            className="rounded-none md:rounded-lg object-bottom"
           />
         </div>
 
