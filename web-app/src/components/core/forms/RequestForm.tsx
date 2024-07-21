0d93406ec2ddd34b17162c71d6b7337b12c8fcbd
chvaldez10 <christianvaldez021@gmail.com>
2024-07-21 10:45:51 -0600

    added text input

--- web-app/src/components/core/forms/RequestForm.tsx
+++ web-app/src/components/core/forms/RequestForm.tsx
@@ -1,5 +1,6 @@
 import { FC, useState } from "react";
 import LoginInput from "./atoms/LoginInput";
+import TextInput from "./atoms/TextInput";
 
 const RequestForm: FC = () => {
   const [formData, setFormData] = useState({
@@ -27,78 +28,16 @@ const RequestForm: FC = () => {
 
   return (
     <form onSubmit={handleSubmit} className="space-y-4">
-      <div className="relative z-0 w-full mb-5 group">
-        <input
-          type="email"
-          name="floating_email"
-          id="floating_email"
-          className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none focus:outline-none focus:ring-0 focus:border-purple-500 peer"
-          placeholder=" "
-          required
-        />
-        <label
-          htmlFor="floating_email"
-          className="peer-focus:font-medium absolute text-sm text-gray-500 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto peer-focus:text-purple-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
-        >
-          Email address
-        </label>
-      </div>
-      <div>
-        <label
-          htmlFor="fullName"
-          className="block text-sm font-medium text-gray-700"
-        >
-          Full Name (optional)
-        </label>
-        <input
-          type="text"
-          name="fullName"
-          id="fullName"
-          value={formData.fullName}
-          onChange={handleChange}
-          className="mt-1 block w-full border border-gray-300 rounded-md shadow-sm focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
-        />
+      <TextInput type="text" name="fullName" id="fullName" label="Full Name" />
+      <TextInput type="email" name="email" id="email" label="Email" required />
+      <TextInput
+        type="tel"
+        name="phoneNumber"
+        id="phoneNumber"
+        label="Phone Number"
+        maxLength={10}
+      />
 
-        <LoginInput
-          type="text"
-          name="fullName"
-          id="fullName"
-          placeholder="Full Name"
-        />
-      </div>
-      <div>
-        <label
-          htmlFor="phoneNumber"
-          className="block text-sm font-medium text-gray-700"
-        >
-          Phone Number (optional)
-        </label>
-        <input
-          type="text"
-          name="phoneNumber"
-          id="phoneNumber"
-          value={formData.phoneNumber}
-          onChange={handleChange}
-          className="mt-1 block w-full border border-gray-300 rounded-md shadow-sm focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
-        />
-      </div>
-      <div>
-        <label
-          htmlFor="email"
-          className="block text-sm font-medium text-gray-700"
-        >
-          Email (required)
-        </label>
-        <input
-          type="email"
-          name="email"
-          id="email"
-          value={formData.email}
-          onChange={handleChange}
-          required
-          className="mt-1 block w-full border border-gray-300 rounded-md shadow-sm focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
-        />
-      </div>
       <div>
         <label
           htmlFor="option"
---
+++ web-app/src/components/core/forms/atoms/TextInput.tsx
@@ -0,0 +1,68 @@
+import { FC, useState } from "react";
+
+interface TextInputProps {
+  type: string;
+  name: string;
+  id: string;
+  label: string;
+  required?: boolean;
+  maxLength?: number;
+}
+
+const TextInput: FC<TextInputProps> = ({
+  type,
+  name,
+  id,
+  label,
+  required = false,
+  maxLength,
+}) => {
+  const [value, setValue] = useState("");
+
+  const handleKeyPress = (event: React.KeyboardEvent<HTMLInputElement>) => {
+    if (type === "tel") {
+      const key = event.key;
+      if (!/^\d$/.test(key) && key !== "Backspace") {
+        event.preventDefault();
+      }
+    }
+  };
+
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const inputValue = event.target.value;
+    setValue(inputValue);
+  };
+
+  const isEmailValid = (email: string) => {
+    const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+    return emailRegex.test(email);
+  };
+
+  return (
+    <div className="relative z-0 w-full mb-5 group">
+      <input
+        type={type}
+        name={name}
+        id={id}
+        className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none focus:outline-none focus:ring-0 focus:border-purple-500 peer"
+        placeholder=" "
+        required={required}
+        onKeyDown={handleKeyPress}
+        onChange={handleChange}
+        maxLength={maxLength}
+        value={value}
+      />
+      <label
+        htmlFor={id}
+        className="peer-focus:font-medium absolute text-sm text-gray-500 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto peer-focus:text-purple-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
+      >
+        {label}
+      </label>
+      {type === "email" && !isEmailValid(value) && value !== "" && (
+        <p className="text-xs mt-1">Invalid email address</p>
+      )}
+    </div>
+  );
+};
+
+export default TextInput;
