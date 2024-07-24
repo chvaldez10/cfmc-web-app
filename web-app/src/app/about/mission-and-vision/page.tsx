6c29395c12b2f5f2d5982c55a5900ade4475dc6d
chvaldez10 <christianvaldez021@gmail.com>
2024-07-23 22:09:49 -0600

    added template from tailblocks

--- web-app/src/app/about/mission-and-vision/page.tsx
+++ web-app/src/app/about/mission-and-vision/page.tsx
@@ -1,14 +1,34 @@
-import HeroOverlappingImage from "@/components/core/hero/HeroOverlappingImage";
-import MissionStatement from "@/data/hero/MissionStatement";
-import { missionStatementLong } from "@/data/hero/constants";
+import { HeroOverlappingImage, HeroSlideClient } from "@/components/core/hero";
+import { NextImage } from "@/components/core/gallery";
+import {
+  missionStatementLong,
+  visionStatementLong,
+} from "@/data/hero/constants";
 
 export default function page() {
   return (
-    <HeroOverlappingImage
-      header={"Welcome to Our Church"}
-      subheader={"Mission Statement"}
-      imagePath={"/background/Bible_with_leaf_resized.jpg"}
-      element={missionStatementLong}
-    />
+    <>
+      <HeroOverlappingImage
+        header={"Welcome to Our Church"}
+        subheader={"Mission Statement"}
+        imagePath={"/background/Bible_with_leaf_resized.jpg"}
+        element={missionStatementLong}
+      />
+
+      <HeroSlideClient
+        header={"Our Vision"}
+        longParagraph={visionStatementLong}
+        element={
+          <NextImage
+            width={"w-full"}
+            height={"h-96 md:h-[50vh] 2xl:h-[55vh]"}
+            src={"/featured/past-events-1/2.jpg"}
+            alt={"Next Big Event Image"}
+            imageClassName={"rounded-lg object-center"}
+          />
+        }
+        reverse={true}
+      />
+    </>
   );
 }
