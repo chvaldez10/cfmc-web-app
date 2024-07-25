65e6db67f3bd713d6df68a7c8d40838acc410db0
chvaldez10 <christianvaldez021@gmail.com>
2024-07-24 22:30:48 -0600

    added vertical list styling

--- web-app/src/components/core/hero/VerticalList.tsx
+++ web-app/src/components/core/hero/VerticalList.tsx
@@ -2,6 +2,7 @@ import { FC } from "react";
 import Link from "next/link";
 import { liturgyTableData } from "@/types/genericTypes";
 import SectionHeader from "../text/SectionHeader";
+import { SectionSubheader, LongParagraph } from "../text";
 
 interface VerticalListProps {
   header: string;
@@ -14,18 +15,16 @@ const VerticalList: FC<VerticalListProps> = ({ header, data }) => {
       {/* Time slots */}
       <div className="divide-y divide-purple-300">
         {data.map((dataEntry, index) => (
-          <div key={index} className="py-8 flex flex-wrap md:flex-nowrap">
+          <div key={index} className="py-6 flex flex-wrap md:flex-nowrap">
             <div className="md:w-64 mb-6 md:mb-0 flex-shrink-0 flex flex-col">
-              <span className="font-semibold title-font text-purple-500">
+              <span className="font-semibold title-font text-gray-900">
                 Date
               </span>
               <span className="mt-1 text-gray-500 text-sm">12 Jun 2024</span>
             </div>
-            <div className="md:flex-grow">
-              <h2 className="text-2xl font-medium text-purple-500 title-font mb-2">
-                Header
-              </h2>
-              <p className="leading-relaxed">Long Paragraph</p>
+            <div className="md:flex-grow text-gray-900">
+              <SectionSubheader text={"Header"} />
+              <LongParagraph> Long Paragraph</LongParagraph>
             </div>
           </div>
         ))}
