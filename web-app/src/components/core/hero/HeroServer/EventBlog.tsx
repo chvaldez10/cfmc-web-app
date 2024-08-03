1504a5c0178beb93c88cc805147791744ade1ec1
chvaldez10 <christianvaldez021@gmail.com>
2024-08-03 17:46:53 -0600

    comment out eventblog

--- web-app/src/components/core/hero/HeroServer/EventBlog.tsx
+++ web-app/src/components/core/hero/HeroServer/EventBlog.tsx
@@ -1,28 +1,28 @@
-import { FC } from "react";
-import { EventBlogCategory } from "@/components/core/cards";
-import { EventData } from "@/types/eventData";
-import SectionHeader from "@/components/core/text/SectionHeader";
-import ContainerWrapper from "@/components/core/hero/molecules/ContainerWrapper";
+// import { FC } from "react";
+// import { EventBlogCategory } from "@/components/core/cards";
+// import { EventData } from "@/types/eventData";
+// import SectionHeader from "@/components/core/text/SectionHeader";
+// import ContainerWrapper from "@/components/core/hero/molecules/ContainerWrapper";
 
-interface EventBlogProps {
-  events: EventData[];
-}
+// interface EventBlogProps {
+//   events: EventData[];
+// }
 
-const EventBlog: FC<EventBlogProps> = ({ events }) => {
-  return (
-    <div className="div-outside-width py-16 gap-5 bg-violet-10">
-      <SectionHeader text={"All Upcoming Events"} className={"text-center"} />
+// const EventBlog: FC<EventBlogProps> = ({ events }) => {
+//   return (
+//     <div className="div-outside-width py-16 gap-5 bg-violet-10">
+//       <SectionHeader text={"All Upcoming Events"} className={"text-center"} />
 
-      {/* Events */}
-      <ContainerWrapper>
-        <div className="flex flex-wrap -mx-4 ">
-          {events.map((event, index) => (
-            <EventBlogCategory key={index} event={event} />
-          ))}
-        </div>
-      </ContainerWrapper>
-    </div>
-  );
-};
+//       {/* Events */}
+//       <ContainerWrapper>
+//         <div className="flex flex-wrap -mx-4 ">
+//           {events.map((event, index) => (
+//             <EventBlogCategory key={index} event={event} />
+//           ))}
+//         </div>
+//       </ContainerWrapper>
+//     </div>
+//   );
+// };
 
-export default EventBlog;
+// export default EventBlog;
