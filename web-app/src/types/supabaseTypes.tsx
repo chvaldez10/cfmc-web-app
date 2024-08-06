c450769f64127bba37ccd3d4be523889555e49e5
chvaldez10 <christianvaldez021@gmail.com>
2024-08-06 11:24:26 -0600

    modified test event data

--- web-app/src/types/supabaseTypes.tsx
+++ web-app/src/types/supabaseTypes.tsx
@@ -1,21 +1,3 @@
-export interface EventData {
-  id: number;
-  name: string;
-  startDate: Date;
-  endDate: Date;
-  updatedAt: Date;
-  createdAt: Date;
-  tags: string[]; // similar to category with more flexibility
-  category: string; // similar to tags but with more specific categories
-  occurrence: string; // multiple-days,weekly, monthly, one-time
-  description: string;
-  image: string;
-  location: string;
-  status: string;
-  organizerName: string;
-  slug: string;
-}
-
 export interface EventDataProps {
   id?: number;
   name: string;
