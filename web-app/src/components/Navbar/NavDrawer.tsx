ee81b845dc923f852bce4ebb941d11b48d3fce0a
chvaldez10 <christianvaldez021@gmail.com>
2024-08-12 21:11:16 -0600

    clean nav drawer

--- web-app/src/components/Navbar/NavDrawer.tsx
+++ web-app/src/components/Navbar/NavDrawer.tsx
@@ -1,5 +1,6 @@
 import { FC } from "react";
 import NavbarLinks from "./NavbarLinks";
+import { Box, FlexCenter } from "@/components/core/ui";
 import { CloseButton } from "@/components/core/ui";
 
 interface SidebarProps {
@@ -9,20 +10,20 @@ interface SidebarProps {
 
 const NavDrawer: FC<SidebarProps> = ({ isOpen, toggleMenu }) => {
   return (
-    <div
-      className={`fixed inset-0 bg-white-0 z-50 transition-transform transform ${
+    <Box
+      containerClassName={`fixed inset-0 bg-white-0 z-50 transition-transform transform ${
         isOpen
           ? "translate-x-0 transition ease-out duration-300"
           : "translate-x-full ease-in duration-300"
       }`}
     >
-      <div className="absolute top-7 left-7">
+      <Box containerClassName="absolute top-7 left-7">
         <CloseButton onClose={toggleMenu} />
-      </div>
-      <div className="div-outside-width h-screen">
+      </Box>
+      <FlexCenter containerClassName="div-outside-width min-h-screen">
         <NavbarLinks />
-      </div>
-    </div>
+      </FlexCenter>
+    </Box>
   );
 };
 
