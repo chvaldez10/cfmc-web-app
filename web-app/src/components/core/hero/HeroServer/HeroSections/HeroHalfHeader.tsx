31aa4c7c27658a95f4ec1be1955858a5fa20c323
chvaldez10 <christianvaldez021@gmail.com>
2024-08-13 22:08:58 -0600

    remove default styling

--- web-app/src/components/core/hero/HeroServer/HeroSections/HeroHalfHeader.tsx
+++ web-app/src/components/core/hero/HeroServer/HeroSections/HeroHalfHeader.tsx
@@ -1,50 +1,50 @@
-import Image from "next/image";
-import Link from "next/link";
-import { FaBolt, FaShieldAlt, FaBrain } from "react-icons/fa";
-import { Box } from "@/components/core/ui";
+import { FaUserFriends, FaYoutube, FaBrain } from "react-icons/fa";
+import {
+  Box,
+  FlexCenter,
+  SectionHeader,
+  LongParagraph,
+  SectionSubheader,
+} from "@/components/core/ui";
 import SVGWage from "@/components/core/hero/molecules/atoms/SVGWage";
 
 const HeroHalfHeader = () => {
   return (
     <Box containerClassName="pt-20 relative bg-gradient-to-r from-white-10 to-green-10 text-white overflow-hidden">
-      <div className="container mx-auto px-4 py-24 md:py-32 relative z-10">
-        <div className="flex flex-col md:flex-row items-center justify-between">
-          <div className="w-full md:w-1/2 mb-12 md:mb-0">
-            <h1 className="text-5xl md:text-6xl font-bold mb-6 leading-tight">
-              Get Involved
-              <br />
-              In Multimedia
-              <br />
-              Ministry
-            </h1>
-            <p className="text-xl mb-8 text-gray-600">
+      <Box containerClassName="container mx-auto px-4 py-24 md:py-32 relative z-10">
+        <FlexCenter containerClassName="md:flex-row ">
+          <Box containerClassName="w-full md:w-1/2 mb-12 md:mb-0 space-y-8">
+            <SectionHeader
+              text="Multimedia Ministry"
+              containerClassName="text-5xl lg:text-6xl xl:text-7xl font-bold"
+            />
+            <LongParagraph>
               Some super epic text about getting involved in multimedia
               ministry.
-            </p>
-          </div>
+            </LongParagraph>
+          </Box>
 
-          {/* Right Side: Features */}
-          <div className="w-full md:w-1/2 md:pl-12">
-            <div className="bg-white bg-opacity-10 backdrop-filter backdrop-blur-lg rounded-xl p-8 shadow-2xl">
-              <h2 className="text-2xl font-semibold mb-6">Join The Team</h2>
+          <Box containerClassName="w-full md:w-1/2 md:pl-12 ">
+            <Box containerClassName="bg-white-0 bg-opacity-10 backdrop-filter backdrop-blur-lg rounded-xl p-8 shadow-2xl space-y-4">
+              <SectionSubheader text="Join The Ministry" />
               <ul className="space-y-4">
                 <li className="flex items-center">
-                  <FaBolt className="w-6 h-6 mr-3 text-yellow-400" />
-                  <span>Learn Something Cool</span>
+                  <FaYoutube className="w-6 h-6 mr-3 text-red-500" />
+                  <span>Learn to Play</span>
                 </li>
                 <li className="flex items-center">
-                  <FaShieldAlt className="w-6 h-6 mr-3 text-green-400" />
-                  <span>Make New Friends</span>
+                  <FaUserFriends className="w-6 h-6 mr-3 text-green-500" />
+                  <span>Meet New Friends</span>
                 </li>
                 <li className="flex items-center">
-                  <FaBrain className="w-6 h-6 mr-3 text-purple-400" />
+                  <FaBrain className="w-6 h-6 mr-3 text-purple-500" />
                   <span>Be Creative</span>
                 </li>
               </ul>
-            </div>
-          </div>
-        </div>
-      </div>
+            </Box>
+          </Box>
+        </FlexCenter>
+      </Box>
 
       <SVGWage />
     </Box>
