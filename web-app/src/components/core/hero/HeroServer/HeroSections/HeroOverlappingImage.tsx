8feab4792b5e2ebe21d768bcad781e7b88044aca
chvaldez10 <christianvaldez021@gmail.com>
2024-08-16 22:51:06 -0600

    hero overlapping image cleanup

--- web-app/src/components/core/hero/HeroServer/HeroSections/HeroOverlappingImage.tsx
+++ web-app/src/components/core/hero/HeroServer/HeroSections/HeroOverlappingImage.tsx
@@ -4,6 +4,9 @@ import {
   SectionHeader,
   SectionSubheader,
   LongParagraph,
+  FlexCenter,
+  Box,
+  Flex,
 } from "@/components/core/ui";
 import { HeroContentProps } from "@/types/genericTypes";
 
@@ -18,11 +21,10 @@ const HeroOverlappingImage: FC<HeroOverlappingImageProps> = ({
   imagePath,
   longParagraph,
 }) => {
-  // Note that the layout for this hero section is a bit hard to refactor and make it similar to the other hero sections.
   return (
-    <div className="relative div-outside-width mx-auto lg:flex-row-reverse lg:max-w-5xl h-screen">
+    <FlexCenter containerClassName="relative mx-auto lg:flex-row-reverse lg:max-w-5xl h-screen">
       {/* Image Column */}
-      <div className="hidden w-576 h-576 lg:block lg:ml-auto">
+      <Box containerClassName="hidden w-576 h-576 lg:block lg:ml-auto">
         <Image
           src={imagePath}
           alt="Hero Overlapping Image"
@@ -31,25 +33,20 @@ const HeroOverlappingImage: FC<HeroOverlappingImageProps> = ({
           style={{ objectFit: "cover" }}
           className="w-full h-full"
         />
-      </div>
+      </Box>
 
       {/* Text Column */}
-      <div className="relative md:absolute md:max-w-2xl lg:left-0 z-10 bg-white-10 lg:bg-white-0 lg:shadow-lg lg:rounded-lg">
-        <div className="flex flex-col p-4 md:p-6 lg:p-12">
-          {/* Header */}
+      <Box containerClassName="relative md:absolute md:max-w-2xl lg:left-0 z-10 bg-white-10 lg:bg-white-0 lg:shadow-lg lg:rounded-lg">
+        <Flex containerClassName="flex flex-col p-4 md:p-6 lg:p-12 space-y-2">
           <SectionHeader text={header} containerClassName="xl:text-5xl" />
-
-          {/* Header */}
           <SectionSubheader
             text={subheader}
             containerClassName="font-bold text-gray-800 text-xl"
           />
-
-          {/* Long Paragraph */}
           <LongParagraph>{longParagraph}</LongParagraph>
-        </div>
-      </div>
-    </div>
+        </Flex>
+      </Box>
+    </FlexCenter>
   );
 };
 
