a78e55b81f62678a96fd7ed57a4295e2ec98dbe3
chvaldez10 <christianvaldez021@gmail.com>
2024-08-17 12:16:42 -0600

    fix event map styling

--- web-app/src/components/core/hero/molecules/events/EventGoogleMaps.tsx
+++ web-app/src/components/core/hero/molecules/events/EventGoogleMaps.tsx
@@ -8,8 +8,8 @@ interface EventDescriptionProps {
 
 const EventGoogleMaps: FC<EventDescriptionProps> = ({ eventLocation }) => {
   return (
-    <Box containerClassName="resize-hero-width mx-auto">
-      <HeroHalfWrapper containerClassName=" h-96">
+    <Box containerClassName="resize-hero-width mx-auto p-4 lg:py-4 lg:px-0">
+      <HeroHalfWrapper containerClassName="h-56 md:h-96">
         <IframeMap src={eventLocation} />
       </HeroHalfWrapper>
     </Box>
