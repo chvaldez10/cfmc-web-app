f37542d5b5c227474ae134b29d28982e48c2091d
chvaldez10 <christianvaldez021@gmail.com>
2024-08-17 14:03:34 -0600

    wordwall cleanup

--- web-app/src/components/core/hero/HeroServer/WordWall.tsx
+++ web-app/src/components/core/hero/HeroServer/WordWall.tsx
@@ -1,7 +1,8 @@
-import { FC } from "react";
 import { SectionHeader } from "@/components/core/ui";
 import { membersBirthday } from "@/data/testData/membersBirthday";
 import { getChurchMembersBirthdayForMonth } from "@/utils/supabase/churchMembersActions";
+import { FlexCenter, Flex } from "@/components/core/ui";
+import { ContainerWrapper } from "@/components/core/hero";
 import BallonItem from "../molecules/atoms/BalloonItem";
 
 async function WordWall() {
@@ -12,23 +13,19 @@ async function WordWall() {
     currentYear
   );
 
-  console.log(currentMonth, currentYear);
-
   console.log(membersBirthdayTest);
 
   return (
-    <div className="div-outside-width p-4">
-      <div className="resize-hero-width">
-        <SectionHeader text={"Birthdays"} containerClassName="text-center" />
-        <div className="container px-5 py-8 md:py-14 mx-auto">
-          <div className="flex flex-wrap justify-between -mx-4 -my-8">
-            {membersBirthday.map((memberBirthday, index) => (
-              <BallonItem key={index} {...memberBirthday} />
-            ))}
-          </div>
-        </div>
-      </div>
-    </div>
+    <FlexCenter containerClassName="p-4 resize-hero-width mx-auto">
+      <SectionHeader text={"Birthdays"} containerClassName="text-center" />
+      <ContainerWrapper containerClassName="py-8 md:py-14">
+        <Flex containerClassName="flex flex-wrap justify-between -mx-4 -my-8">
+          {membersBirthday.map((memberBirthday, index) => (
+            <BallonItem key={index} {...memberBirthday} />
+          ))}
+        </Flex>
+      </ContainerWrapper>
+    </FlexCenter>
   );
 }
 
