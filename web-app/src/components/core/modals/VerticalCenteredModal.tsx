21289d1d1846a0731067873bde3a4fa2ab8b8ba2
chvaldez10 <christianvaldez021@gmail.com>
2024-08-21 10:09:52 -0600

    update box component for forward ref

--- web-app/src/components/core/modals/VerticalCenteredModal.tsx
+++ web-app/src/components/core/modals/VerticalCenteredModal.tsx
@@ -1,8 +1,8 @@
-import { forwardRef, ReactNode } from "react";
+import { forwardRef } from "react";
 import { SectionSubheader } from "@/components/core/ui";
 import { CloseButton } from "@/components/core/ui";
 import { ModalProps } from "@/types/genericTypes";
-import { FlexCenter, Box } from "@/components/core/ui";
+import { FlexCenter, Box, Flex } from "@/components/core/ui";
 
 interface VerticalCenteredModalProps extends ModalProps {
   onClose: () => void;
@@ -12,19 +12,18 @@ const VerticalCenteredModal = forwardRef<
   HTMLDivElement,
   VerticalCenteredModalProps
 >(({ children, title, onClose }, modalRef) => {
-  // TODO: Improve styling
   return (
     <FlexCenter containerClassName="fixed inset-0 z-30 overflow-x-hidden overflow-y-auto">
-      <div
-        className="resize-modal border border-gray-200 rounded-lg bg-white-0 animate-translate-up text-gray-900"
+      <Box
+        containerClassName="resize-modal border border-gray-200 rounded-lg bg-white-0 animate-translate-up text-gray-900"
         ref={modalRef}
       >
-        <div className="div-outside-width-justify-between px-5 py-3 border-b border-gray-200 bg-purple-50 md:rounded-t-lg">
+        <Flex containerClassName="div-outside-width-justify-between px-5 py-3 border-b border-gray-200 bg-purple-50 md:rounded-t-lg">
           <SectionSubheader text={title} />
           <CloseButton onClose={onClose} />
-        </div>
-        <div className="p-5">{children}</div>
-      </div>
+        </Flex>
+        <Box containerClassName="p-5">{children}</Box>
+      </Box>
     </FlexCenter>
   );
 });
--- web-app/src/components/core/ui/layout/Box.tsx
+++ web-app/src/components/core/ui/layout/Box.tsx
@@ -1,12 +1,14 @@
-import { FC } from "react";
+import { FC, ForwardedRef, forwardRef } from "react";
 import { DivProps } from "@/types/genericTypes";
 
-interface BoxProps extends DivProps {}
+interface BoxProps extends DivProps {
+  ref?: ForwardedRef<HTMLDivElement>;
+}
 
 // Note that the Box component has no default styles. Its just to have better visualization of the layout when coding.
-const Box: FC<BoxProps> = ({ children, containerClassName, style }) => {
+const Box: FC<BoxProps> = ({ children, containerClassName, style, ref }) => {
   return (
-    <div className={containerClassName} style={style}>
+    <div className={containerClassName} style={style} ref={ref}>
       {children}
     </div>
   );
