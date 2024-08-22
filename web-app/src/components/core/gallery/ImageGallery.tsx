31da480435fbdb10555924d328ad232104492642
chvaldez10 <christianvaldez021@gmail.com>
2024-08-22 11:04:30 -0600

    refactor layout

--- web-app/src/components/core/gallery/ImageGallery.tsx
+++ web-app/src/components/core/gallery/ImageGallery.tsx
@@ -1,83 +1,74 @@
 import { FC } from "react";
 import {
-  Flex,
   SectionSubheader,
   LongParagraph,
   Box,
+  Flex,
 } from "@/components/core/ui";
 import { NextImage } from "@/components/core/gallery";
-import Image from "next/image";
 import { pastEvents1 } from "@/data/hero";
 
 const ImageGallery: FC = () => {
   return (
-    <Flex containerClassName="container px-5 py-24 mx-auto flex flex-wrap resize-hero-width space-y-4">
-      <Flex containerClassName="flex flex-wrap items-center w-full space-x-4">
+    <div className="container px-5 py-24 mx-auto space-y-4 resize-hero-width">
+      <Box>
         <SectionSubheader text="Past events" />
-        <LongParagraph containerClassName="">
+        <LongParagraph>
           {`Check out some of the past events we've hosted.`}
         </LongParagraph>
-      </Flex>
-      <Flex containerClassName="flex flex-wrap md:-m-2 -m-1 ">
-        <Flex containerClassName="flex flex-wrap w-1/2">
-          <Box containerClassName="md:p-2 p-1 w-1/2">
-            <NextImage
-              alt={pastEvents1[0].alt}
-              height={"h-48"}
-              src={pastEvents1[0].src}
-              containerClassName="object-cover object-center"
-              imageClassName="rounded-lg object-center"
-            />
-          </Box>
-          <Box containerClassName="md:p-2 p-1 w-1/2">
-            <NextImage
-              alt={pastEvents1[0].alt}
-              height={"h-48"}
-              src={pastEvents1[0].src}
-              containerClassName="object-cover object-center "
-              imageClassName="rounded-lg object-center"
-            />
-          </Box>
+      </Box>
+
+      <Flex containerClassName="flex flex-row gap-2">
+        <div className="grid w-full md:w-1/2 grid-cols-2 grid-rows-2 gap-2">
           <NextImage
-            alt={pastEvents1[0].alt}
-            height={"h-96"}
-            width={"w-full"}
+            height={"h-56"}
             src={pastEvents1[0].src}
-            containerClassName="object-cover object-center md:p-2 p-1"
+            alt={pastEvents1[0].alt}
+            containerClassName="col-span-1 row-span-1"
             imageClassName="rounded-lg object-center"
           />
-        </Flex>
-        <Flex containerClassName="flex flex-wrap w-1/2">
-          <Box containerClassName="md:p-2 p-1 w-full">
-            <NextImage
-              alt={pastEvents1[0].alt}
-              height={"h-96"}
-              src={pastEvents1[0].src}
-              containerClassName="object-cover object-center"
-              imageClassName="rounded-lg object-center"
-            />
-          </Box>
-          <Box containerClassName="md:p-2 p-1 w-1/2">
-            <Image
-              alt="gallery"
-              className="object-cover object-center"
-              src="https://images.unsplash.com/photo-1492684223066-81342ee5ff30?q=80&w=1770&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-              width={502}
-              height={302}
-            />
-          </Box>
-          <Box containerClassName="md:p-2 p-1 w-1/2">
-            <Image
-              alt="gallery"
-              className="object-cover object-center"
-              src="https://images.unsplash.com/photo-1492684223066-81342ee5ff30?q=80&w=1770&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-              width={503}
-              height={303}
-            />
-          </Box>
-        </Flex>
+          <NextImage
+            alt={pastEvents1[1].alt}
+            height={"h-56"}
+            width={"w-full"}
+            src={pastEvents1[1].src}
+            containerClassName="col-span-1 row-span-1"
+            imageClassName="rounded-lg object-center"
+          />
+          <NextImage
+            alt={pastEvents1[2].alt}
+            height={"h-56"}
+            src={pastEvents1[2].src}
+            containerClassName="col-span-2 row-span-1"
+            imageClassName="rounded-lg object-center"
+          />
+        </div>
+        <div className="hidden md:grid w-full md:w-1/2 grid-cols-2 grid-rows-2 gap-2">
+          <NextImage
+            alt={pastEvents1[2].alt}
+            height={"h-56"}
+            src={pastEvents1[2].src}
+            containerClassName="col-span-2 row-span-1"
+            imageClassName="rounded-lg object-center"
+          />
+          <NextImage
+            height={"h-56"}
+            src={pastEvents1[0].src}
+            alt={pastEvents1[0].alt}
+            containerClassName="col-span-1 row-span-1"
+            imageClassName="rounded-lg object-center"
+          />
+          <NextImage
+            alt={pastEvents1[1].alt}
+            height={"h-56"}
+            width={"w-full"}
+            src={pastEvents1[1].src}
+            containerClassName="col-span-1 row-span-1"
+            imageClassName="rounded-lg object-center"
+          />
+        </div>
       </Flex>
-    </Flex>
+    </div>
   );
 };
 
