7c81f592e96bbe253158a3809acc497be71ab120
chvaldez10 <christianvaldez021@gmail.com>
2024-08-25 10:43:11 -0600

    create imagecontentprofile

--- web-app/src/components/core/hero/molecules/multimedia/ImageContent.tsx
+++ web-app/src/components/core/hero/molecules/multimedia/ImageContent.tsx
@@ -1,5 +1,4 @@
 import { FC } from "react";
-import Link from "next/link";
 import {
   Flex,
   Box,
@@ -8,6 +7,7 @@ import {
   LongParagraph,
   LearnMoreButton,
 } from "@/components/core/ui";
+import ImageContentProfile from "./ImageContentProfile";
 import { IoPerson } from "react-icons/io5";
 import { NextImage } from "@/components/core/gallery";
 
@@ -23,22 +23,7 @@ const ImageContent: FC = () => {
           imageClassName={"rounded-lg object-center"}
         />
         <Flex containerClassName="flex flex-col sm:flex-row mt-10">
-          <Box containerClassName="sm:w-1/3 text-center sm:pr-8 sm:py-8 space-y-4">
-            <Box containerClassName="w-20 h-20 rounded-full inline-flex items-center justify-center bg-purple-100 text-purple-500">
-              <IoPerson className="w-10 h-10" />
-            </Box>
-            <FlexCenter containerClassName="text-center ">
-              <SectionSubheader
-                containerClassName="title-font text-gray-900"
-                text="Phoebe Caulfield"
-              />
-              <Box containerClassName="w-12 h-1 bg-purple-500 rounded mt-2 mb-4"></Box>
-              <LongParagraph containerClassName="text-gray-600">
-                Raclette knausgaard hella meggs normcore williamsburg enamel pin
-                sartorial venmo tbh hot chicken gentrify portland.
-              </LongParagraph>
-            </FlexCenter>
-          </Box>
+          <ImageContentProfile />
           <Flex containerClassName="sm:w-2/3 sm:pl-8 sm:py-8 sm:border-l border-gray-200 sm:border-t-0 border-t mt-4 pt-4 sm:mt-0 text-center sm:text-left flex flex-col ">
             <LongParagraph containerClassName="text-gray-600">
               Super long content description goes here.
---
+++ web-app/src/components/core/hero/molecules/multimedia/ImageContentProfile.tsx
@@ -0,0 +1,36 @@
+import { FC } from "react";
+import { IoPerson } from "react-icons/io5";
+import {
+  Box,
+  FlexCenter,
+  SectionSubheader,
+  LongParagraph,
+} from "@/components/core/ui";
+import { ChurchMembers } from "@/types/supabaseTypes";
+
+// interface ImageContentProfileProps extends ChurchMembers {}
+
+const ImageContentProfile: FC = ({}) => {
+  return (
+    <>
+      <Box containerClassName="sm:w-1/3 text-center sm:pr-8 sm:py-8 space-y-4">
+        <Box containerClassName="w-20 h-20 rounded-full inline-flex items-center justify-center bg-purple-100 text-purple-500">
+          <IoPerson className="w-10 h-10" />
+        </Box>
+        <FlexCenter containerClassName="text-center ">
+          <SectionSubheader
+            containerClassName="title-font text-gray-900"
+            text="Phoebe Caulfield"
+          />
+          <Box containerClassName="w-12 h-1 bg-purple-500 rounded mt-2 mb-4"></Box>
+          <LongParagraph containerClassName="text-gray-600">
+            Raclette knausgaard hella meggs normcore williamsburg enamel pin
+            sartorial venmo tbh hot chicken gentrify portland.
+          </LongParagraph>
+        </FlexCenter>
+      </Box>
+    </>
+  );
+};
+
+export default ImageContentProfile;
