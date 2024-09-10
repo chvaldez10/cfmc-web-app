0f04c72feafc525020e1de2136d2e5d2fe4afc29
chvaldez10 <christianvaldez021@gmail.com>
2024-09-09 22:55:03 -0600

    remove repeated email + phone number

--- web-app/src/components/core/cards/atoms/FullTeamCard.tsx
+++ web-app/src/components/core/cards/atoms/FullTeamCard.tsx
@@ -24,31 +24,25 @@ const FullTeamCard: FC<FullTeamCardProps> = ({ ...props }) => {
         imageClassName={"rounded-t-lg object-center"}
       />
 
-      <Box containerClassName="w-full max-w-7xl mx-auto px-6 md:px-8">
+      <Box containerClassName="w-full max-w-7xl mx-auto px-6 md:px-8 space-y-4">
         <Flex containerClassName="flex-center sm:justify-start relative z-10 -mt-12">
           <Box containerClassName="bg-white-0 rounded-full p-2 border-4 border-solid border-white-0">
             <IoPerson className="w-24 h-24 text-gray-300" />
           </Box>
         </Flex>
-        <FlexCenter containerClassName="sm:flex-row max-sm:gap-4 sm:justify-between">
-          <Box containerClassName="block">
-            <SectionSubheader
-              text={`${props.firstName} ${props.lastName}`}
-              containerClassName="text-center max-sm:text-center text-start"
-            />
-            <p className="text-sm text-gray-600 max-sm:text-center">
-              {props.role}
-              <br className="hidden sm:block" />
-              {props.headline}
-            </p>
-          </Box>
-        </FlexCenter>
+        <Box containerClassName="text-center sm:text-left">
+          <SectionSubheader
+            text={`${props.firstName} ${props.lastName}`}
+            containerClassName=""
+          />
+          <LongParagraph>{`${props.role} | ${props.headline}`}</LongParagraph>
+        </Box>
 
         <Box containerClassName="w-full h-1 bg-gray-300 rounded my-2"></Box>
         <LongParagraph containerClassName="text-gray-600">
           {props.bio}
         </LongParagraph>
-        <Flex containerClassName="flex max-sm:flex-wrap max-sm:justify-center items-center gap-4 py-2">
+        <Flex containerClassName="flex max-sm:flex-wrap max-sm:justify-center items-center gap-4">
           <PurpleColoredBadge>Media</PurpleColoredBadge>
           <PurpleColoredBadge>Video</PurpleColoredBadge>
           <PurpleColoredBadge>Music</PurpleColoredBadge>
