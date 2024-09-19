429a85e29cfacf83e7573fe27e6768a498455c29
chvaldez10 <christianvaldez021@gmail.com>
2024-09-18 23:16:18 -0600

    add use countdown hook

---
+++ web-app/src/hooks/useCountdown.ts
@@ -0,0 +1,32 @@
+import { useEffect, useState } from "react";
+import { getTimeLeft } from "@/utils/common/dateUtils";
+
+export const useCountdown = (targetDate: Date) => {
+  const [timeLeft, setTimeLeft] = useState(getTimeLeft(targetDate));
+  const [isTimeUp, setIsTimeUp] = useState(false);
+
+  useEffect(() => {
+    if (!targetDate) {
+      return;
+    }
+
+    const updateCountdown = () => {
+      const newTimeLeft = getTimeLeft(targetDate);
+      setTimeLeft(newTimeLeft);
+      const isUp = Object.values(newTimeLeft).every((value) => value <= 0);
+      setIsTimeUp(isUp);
+    };
+
+    // Move this outside the effect to avoid unnecessary re-creation
+    const timer = setInterval(updateCountdown, 1000);
+
+    // Initial update
+    updateCountdown();
+
+    return () => {
+      clearInterval(timer);
+    };
+  }, [targetDate]);
+
+  return { timeLeft, isTimeUp };
+};
