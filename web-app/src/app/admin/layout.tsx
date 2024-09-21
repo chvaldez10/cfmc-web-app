c7e85a3a29af223019efd6c6de355fc4b05befb6
chvaldez10 <christianvaldez021@gmail.com>
2024-09-21 00:09:15 -0600

    add admin route

---
+++ web-app/src/app/admin/layout.tsx
@@ -0,0 +1,35 @@
+import type { Metadata } from "next";
+import { Roboto } from "next/font/google";
+import Providers from "../Providers";
+import "../globals.css";
+
+import BackButton from "@/components/core/ui/buttons/BackButton";
+import Footer from "@/components/Footer/Footer";
+
+const roboto = Roboto({
+  weight: "400",
+  subsets: ["latin"],
+});
+
+export const metadata: Metadata = {
+  title: "CFMC",
+  description: "Calgary Methodist Church Web Page",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={roboto.className}>
+        <Providers>
+          {children}
+          <Footer />
+          <BackButton />
+        </Providers>
+      </body>
+    </html>
+  );
+}
---
+++ web-app/src/app/admin/page.tsx
@@ -0,0 +1,5 @@
+import UnderConstruction from "@/components/template/UnderConstruction";
+
+export default async function page() {
+  return <UnderConstruction />;
+}
