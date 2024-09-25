84b9c9d3a345040ff7502d5a6891de3eb7c2e2ef
chvaldez10 <christianvaldez021@gmail.com>
2024-09-24 21:45:15 -0600

    add share timeouts

--- web-app/src/components/core/molecules/atoms/HeroHomeCollage.tsx
+++ web-app/src/components/core/molecules/atoms/HeroHomeCollage.tsx
@@ -1,11 +1,23 @@
-import React from "react";
+"use client";
+
+import { FC, useEffect, useState } from "react";
 import { Grid } from "@/components/core/ui";
 import { heroCollageImages } from "@/data/hero/featuredItems";
 import { NextImage } from "@/components/core/ui";
+import { SHORT_ANIMATION_DELAY } from "@/data/constants/shared";
 
-interface HeroHomeCollageProps {}
+// TODO: Add props if needed
+const HeroHomeCollage: FC = () => {
+  const [loadedImages, setLoadedImages] = useState<number[]>([]);
 
-const HeroHomeCollage: React.FC<HeroHomeCollageProps> = (props) => {
+  useEffect(() => {
+    heroCollageImages.forEach((image, index) => {
+      setTimeout(() => {
+        setLoadedImages((prev) => [...prev, index]);
+      }, index * SHORT_ANIMATION_DELAY);
+    });
+  }, []);
+
   return (
     <Grid
       containerClassName="hidden-grid-1-3 w-full 3xl:w-7/12"
@@ -14,19 +26,23 @@ const HeroHomeCollage: React.FC<HeroHomeCollageProps> = (props) => {
       {heroCollageImages.map((featuredImage, index) => (
         <div
           key={index}
-          className={"opacity-0 animate-slide-up"}
+          className={`opacity-0 ${
+            loadedImages.includes(index) ? "animate-slide-up" : ""
+          }`}
           style={{
-            animationDelay: `${index * 0.3}s`,
+            animationDelay: `${index * SHORT_ANIMATION_DELAY}ms`,
           }}
         >
-          <NextImage
-            key={index}
-            height={"h-96 md:h-[50vh] 2xl:h-[55vh]"}
-            src={featuredImage.src}
-            alt={featuredImage.alt}
-            containerClassName={featuredImage.containerClassName}
-            imageClassName={"rounded-lg object-top"}
-          />
+          {loadedImages.includes(index) && (
+            <NextImage
+              key={index}
+              height={"h-96 md:h-[50vh] 2xl:h-[55vh]"}
+              src={featuredImage.src}
+              alt={featuredImage.alt}
+              containerClassName={featuredImage.containerClassName}
+              imageClassName={"rounded-lg object-top"}
+            />
+          )}
         </div>
       ))}
     </Grid>
---
+++ web-app/src/data/constants/shared.ts
@@ -0,0 +1,6 @@
+/**
+ * Animation delays
+ */
+export const SHORT_ANIMATION_DELAY = 300;
+export const MEDIUM_ANIMATION_DELAY = 500;
+export const LONG_ANIMATION_DELAY = 700;
