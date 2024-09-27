83fba9bc01c27fda4c5128514a52a0e5d3cd1273
chvaldez10 <christianvaldez021@gmail.com>
2024-09-26 21:53:35 -0600

    fix chatgpt cleanup

--- web-app/src/components/core/hero/HeroServer/HeroSections/HeroTwoImages.tsx
+++ web-app/src/components/core/hero/HeroServer/HeroSections/HeroTwoImages.tsx
@@ -3,12 +3,12 @@ import {
   Grid,
   Flex,
   SectionSubheader,
+  LongParagraph,
   NextImage,
   SectionHeader,
 } from "@/components/core/ui";
 import { visionStatementLong } from "@/data/hero";
 import { FC } from "react";
-import { IMAGE_SIZE_SMALL_HERO } from "@/data/constants/shared";
 
 const HeroTwoImages: FC = () => (
   <Box
@@ -20,25 +20,29 @@ const HeroTwoImages: FC = () => (
       <Grid containerClassName="grid grid-cols-2 gap-2 items-start lg:order-first order-last">
         <Flex containerClassName="flex pt-24 justify-start sm:justify-end lg:justify-center">
           <NextImage
-            width="w-full"
-            height={IMAGE_SIZE_SMALL_HERO}
-            src="https://skdambngwkjvsmkhgswi.supabase.co/storage/v1/object/public/images/ministry/3.jpg"
-            alt="Our Vision Image Bottom"
-            imageClassName="rounded-lg object-center"
+            width={"w-full"}
+            height={"h-60 lg:h-96"}
+            src={
+              "https://skdambngwkjvsmkhgswi.supabase.co/storage/v1/object/public/images/ministry/3.jpg"
+            }
+            alt={"Our Vision Image Bottom"}
+            imageClassName={"rounded-lg object-center"}
           />
         </Flex>
         <NextImage
-          width="w-full"
-          height={IMAGE_SIZE_SMALL_HERO}
-          src="https://skdambngwkjvsmkhgswi.supabase.co/storage/v1/object/public/images/ministry/2.jpg?t=2024-09-27T03%3A30%3A08.492Z"
-          alt="Our Vision Image Top"
-          imageClassName="rounded-lg object-center"
+          width={"w-full"}
+          height={"h-60 lg:h-96"}
+          src={
+            "https://skdambngwkjvsmkhgswi.supabase.co/storage/v1/object/public/images/ministry/2.jpg?t=2024-09-27T03%3A30%3A08.492Z"
+          }
+          alt={"Our Vision Image Top"}
+          imageClassName={"rounded-lg object-center"}
         />
       </Grid>
 
       {/* Vision Statement */}
       <Flex containerClassName="flex flex-col items-center lg:items-start space-y-4">
-        <SectionHeader text="Our Vision" containerClassName="xl:text-5xl" />
+        <SectionHeader text={"Our Vision"} containerClassName="xl:text-5xl" />
         <SectionSubheader
           text={visionStatementLong}
           containerClassName="long-paragraph-text-color"
