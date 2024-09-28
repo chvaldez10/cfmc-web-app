bffbb9073fe73884e122fe73d14fadb8d1149be3
chvaldez10 <christianvaldez021@gmail.com>
2024-09-28 16:20:28 -0600

    remove under construction for community page

--- web-app/src/app/(main)/about/our-community/page.tsx
+++ web-app/src/app/(main)/about/our-community/page.tsx
@@ -1,5 +1,18 @@
-import UnderConstruction from "@/components/template/UnderConstruction";
+import { HeroOverlappingImage } from "@/components/core/hero";
+
+const headerLongParagraph =
+  "Our Community. We are a community of believers who are passionate about serving Jesus Christ.";
 
 export default function page() {
-  return <UnderConstruction />;
+  return (
+    <>
+      <HeroOverlappingImage
+        header={"Our Community"}
+        subheader={"Our Community"}
+        imagePath={"/background/Bible_with_leaf_resized.jpg"}
+        longParagraph={headerLongParagraph}
+        dataTestId={"our-community"}
+      />
+    </>
+  );
 }
