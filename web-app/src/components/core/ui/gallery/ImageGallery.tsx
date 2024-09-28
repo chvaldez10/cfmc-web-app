a094f72d3ff8fc6589ad70576c6e2c0d9ac549be
chvaldez10 <christianvaldez021@gmail.com>
2024-09-28 17:34:53 -0600

    remove loop to render each component

--- web-app/src/components/core/ui/gallery/ImageGallery.tsx
+++ web-app/src/components/core/ui/gallery/ImageGallery.tsx
@@ -23,30 +23,52 @@ const ImageGallery: FC = () => {
       <Flex containerClassName="flex flex-row gap-2">
         {/* Grid 1 */}
         <Grid containerClassName="w-full md:w-1/2 grid grid-cols-2 grid-rows-2 gap-2">
-          {pastEvents2_1.map((image) => (
-            <NextImage
-              key={image.src}
-              src={image.src}
-              alt={image.alt}
-              height={IMAGE_SIZE_SMALL_HERO}
-              containerClassName={image.containerClassName}
-              imageClassName={`${image.imageClassName as string} rounded-lg`}
-            />
-          ))}
+          <NextImage
+            key={pastEvents2_1[0].src}
+            src={pastEvents2_1[0].src}
+            alt={pastEvents2_1[0].alt}
+            height={IMAGE_SIZE_SMALL_HERO}
+            containerClassName={"col-span-1 row-span-1"}
+            imageClassName={"rounded-lg object-center"}
+          />
+          <NextImage
+            key={pastEvents2_1[1].src}
+            src={pastEvents2_1[1].src}
+            alt={pastEvents2_1[1].alt}
+            height={IMAGE_SIZE_SMALL_HERO}
+            containerClassName={"col-span-1 row-span-1"}
+            imageClassName={"rounded-lg object-center"}
+          />
+
+          <NextImage
+            key={pastEvents2_1[2].src}
+            src={pastEvents2_1[2].src}
+            alt={pastEvents2_1[2].alt}
+            height={IMAGE_SIZE_SMALL_HERO}
+            containerClassName={"col-span-2 row-span-1"}
+            imageClassName={"rounded-lg object-center"}
+          />
         </Grid>
 
         {/* Grid 2 */}
         <Grid containerClassName="w-full md:w-1/2 hidden md:grid grid-cols-2 grid-rows-2 gap-2">
-          {pastEvents2_2.map((image) => (
-            <NextImage
-              key={image.src}
-              src={image.src}
-              alt={image.alt}
-              height={IMAGE_SIZE_SMALL_HERO}
-              containerClassName={image.containerClassName}
-              imageClassName={`${image.imageClassName as string} rounded-lg`}
-            />
-          ))}
+          <NextImage
+            key={pastEvents2_2[0].src}
+            src={pastEvents2_2[0].src}
+            alt={pastEvents2_2[0].alt}
+            height={IMAGE_SIZE_SMALL_HERO}
+            containerClassName={"col-span-2 row-span-1"}
+            imageClassName={"rounded-lg object-center"}
+          />
+
+          <NextImage
+            key={pastEvents2_2[1].src}
+            src={pastEvents2_2[1].src}
+            alt={pastEvents2_2[1].alt}
+            height={IMAGE_SIZE_SMALL_HERO}
+            containerClassName={"col-span-2 row-span-1"}
+            imageClassName={"rounded-lg object-bottom"}
+          />
         </Grid>
       </Flex>
     </Box>
