33b392bfba4f8e26b6c232906011531acca4d0e2
chvaldez10 <christianvaldez021@gmail.com>
2024-10-05 20:53:22 -0600

    added filler

--- web-app/src/app/(main)/about/mission-and-vision/page.tsx
+++ web-app/src/app/(main)/about/mission-and-vision/page.tsx
@@ -1,6 +1,11 @@
-import { HeroOverlappingImage } from "@/components/core/hero";
+import {
+  HeroTwoImages,
+  HeroSlideClient,
+  HeroOverlappingImage,
+} from "@/components/core/hero";
+import { NextImage } from "@/components/core/ui";
 import { missionStatementLong } from "@/data/hero/";
-import { HeroTwoImages } from "@/components/core/hero";
+import { IMAGE_SIZE_LARGE_HERO } from "@/data/constants/shared";
 
 export default function page() {
   return (
@@ -12,7 +17,26 @@ export default function page() {
         longParagraph={missionStatementLong}
         dataTestId={"mission-statement"}
       />
+
       <HeroTwoImages />
+
+      <HeroSlideClient
+        header={"Who We Are"}
+        longParagraph={"Fill description here"}
+        containerClassName="min-h-screen bg-gradient-to-t from-white-10 via-purple-100 to-purple-200"
+        reverse={true}
+        dataTestId={"next-big-event"}
+      >
+        <NextImage
+          width={"w-full"}
+          height={IMAGE_SIZE_LARGE_HERO}
+          src={
+            "https://skdambngwkjvsmkhgswi.supabase.co/storage/v1/object/public/images/ministry/1.jpg"
+          }
+          alt={"Next Big Event Image"}
+          imageClassName={"rounded-lg object-center"}
+        />
+      </HeroSlideClient>
     </>
   );
 }
