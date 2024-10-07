c8dd6f649843ff2ca24a54726940fce51e8b3be8
chvaldez10 <christianvaldez021@gmail.com>
2024-10-07 09:23:09 -0600

    fix: error handling for dateutils

--- web-app/src/utils/common/dateUtils.ts
+++ web-app/src/utils/common/dateUtils.ts
@@ -10,6 +10,11 @@ export const getNextSunday = (): Date => {
 };
 
 export const getTimeLeft = (targetDate: Date) => {
+  // Immediately return if no target date is provided
+  if (!targetDate) {
+    return { days: 0, hours: 0, minutes: 0, seconds: 0 };
+  }
+
   const totalTimeLeft = targetDate.getTime() - new Date().getTime();
   const days = Math.floor(totalTimeLeft / (1000 * 60 * 60 * 24));
   const hours = Math.floor((totalTimeLeft / (1000 * 60 * 60)) % 24);
