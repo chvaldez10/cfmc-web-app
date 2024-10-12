78e7bc54054b8f97ae5d7653a7478397d63a8e66
chvaldez10 <christianvaldez021@gmail.com>
2024-10-11 19:26:29 -0600

    music card cleanup

--- web-app/src/components/core/ui/cards/atoms/MusicCard.tsx
+++ web-app/src/components/core/ui/cards/atoms/MusicCard.tsx
@@ -1,5 +1,11 @@
 import { FC } from "react";
-import { NextImage } from "@/components/core/ui";
+import {
+  Flex,
+  NextImage,
+  Box,
+  LongParagraph,
+  SectionSubheader,
+} from "@/components/core/ui";
 import { FaStepBackward, FaStepForward, FaPause } from "react-icons/fa";
 import { WorshipSongs } from "@/types/supabase/worship";
 
@@ -7,9 +13,9 @@ interface MusicCardProps extends WorshipSongs {}
 
 const MusicCard: FC<MusicCardProps> = ({ imageSrc, songTitle, artist }) => {
   return (
-    <div className="bg-white-0 p-6 rounded-2xl shadow-md w-full h-full flex flex-col justify-between">
+    <Flex containerClassName="bg-white-0 p-6 rounded-2xl shadow-md- wfull h-full flex flex-col justify-between">
       {/* Album Cover */}
-      <div className="mb-4">
+      <Box containerClassName="mb-4">
         <NextImage
           width={"w-full"}
           height={"h-40"}
@@ -17,38 +23,43 @@ const MusicCard: FC<MusicCardProps> = ({ imageSrc, songTitle, artist }) => {
           alt={songTitle}
           imageClassName="rounded-lg object-cover"
         />
-      </div>
+      </Box>
 
       {/* Song Info */}
-      <div className="text-center mb-4">
-        <h2 className="text-xl font-semibold text-gray-800">{songTitle}</h2>
-        <p className="text-gray-600 text-sm">{artist}</p>
-      </div>
+      <Box containerClassName="text-center mb-4">
+        <SectionSubheader text={songTitle} containerClassName="font-bold" />
+        <LongParagraph>{artist}</LongParagraph>
+      </Box>
 
       {/* Music Controls */}
-      <div className="flex-center mb-4">
+      <Flex containerClassName="flex-center mb-4">
+        {/* Previous */}
         <button className="p-2 rounded-full bg-gray-200 hover:bg-gray-300 focus:outline-none transition">
           <FaStepBackward className="w-4 h-4 text-gray-600" />
         </button>
+
+        {/* Play/Pause */}
         <button className="p-3 rounded-full bg-purple-500 hover:bg-purple-600 focus:outline-none mx-4 transition">
           <FaPause className="w-5 h-5 text-white-0" />
         </button>
+
+        {/* Next */}
         <button className="p-2 rounded-full bg-gray-200 hover:bg-gray-300 focus:outline-none transition">
           <FaStepForward className="w-4 h-4 text-gray-600" />
         </button>
-      </div>
+      </Flex>
 
       {/* Progress Bar */}
-      <div className="mb-2">
-        <div className="bg-gray-200 h-2 rounded-full">
-          <div className="bg-purple-500 h-2 rounded-full w-1/2"></div>
-        </div>
-        <div className="flex justify-between mt-1 text-xs text-gray-600">
+      <Box containerClassName="mb-2">
+        <Box containerClassName="bg-gray-200 h-2 rounded-full">
+          <Box containerClassName="bg-purple-500 h-2 rounded-full w-1/2"></Box>
+        </Box>
+        <Flex containerClassName="flex justify-between mt-1 text-xs text-gray-600">
           <span>1:57</span>
           <span>3:53</span>
-        </div>
-      </div>
-    </div>
+        </Flex>
+      </Box>
+    </Flex>
   );
 };
 
