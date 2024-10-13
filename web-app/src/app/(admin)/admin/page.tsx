bee569ba365ecd22ae13980e8285b2e5d86710c2
chvaldez10 <christianvaldez021@gmail.com>
2024-10-12 23:30:34 -0600

    protect dashboard

--- web-app/src/app/(admin)/admin/page.tsx
+++
@@ -1,5 +0,0 @@
-import UnderConstruction from "@/components/template/UnderConstruction";
-
-export default async function page() {
-  return <UnderConstruction />;
-}
--- web-app/src/app/(admin)/dashboard/page.tsx
+++ web-app/src/app/(admin)/dashboard/page.tsx
@@ -1,15 +1,15 @@
-// import { redirect } from "next/navigation";
-// import { createClient } from "@/utils/supabase/server";
+import { redirect } from "next/navigation";
+import { createClient } from "@/utils/supabase/server";
 import UnderConstruction from "@/components/template/UnderConstruction";
 
 export default async function page() {
-  // const supabase = createClient();
+  const supabase = createClient();
 
-  // const { data, error } = await supabase.auth.getUser();
+  const { data, error } = await supabase.auth.getUser();
 
-  // if (error || !data?.user) {
-  //   redirect("/login");
-  // }
+  if (error || !data?.user) {
+    redirect("/login");
+  }
 
   return <UnderConstruction />;
 }
