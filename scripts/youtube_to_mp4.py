53589b4b9e226231d85a741070a1ecbbf3449c2c
chvaldez10 <christianvaldez021@gmail.com>
2024-10-29 22:20:46 -0600

    add download folder

--- scripts/youtube_to_mp4.py
+++ scripts/youtube_to_mp4.py
@@ -1,5 +1,8 @@
 from pytubefix import YouTube
 from pytubefix.cli import on_progress
+import os
+
+CONST_YOUTUBE_URL = "https://www.youtube.com/watch?v=sEAeW4BbEnk&ab_channel=BELIEVERSMELODY"
 
 def download_video(url):
     try:
@@ -31,8 +34,20 @@ def download_video(url):
         print(f"❌ Error: {str(e)}")
         return False
 
+def _create_downloads_dir_if_not_exists():
+    downloads_dir = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'downloads')
+
+    # Create download directory if not exists
+    if not os.path.exists(downloads_dir):
+        print(f"Creating downloads directory: {downloads_dir}")
+        os.makedirs(downloads_dir, exist_ok=True)
+    
+    return downloads_dir
 
 # Main
 if __name__ == "__main__":
-    url = "https://www.youtube.com/watch?v=sEAeW4BbEnk&ab_channel=BELIEVERSMELODY"
-    download_video(url)
+    # Create download directory if not exists
+    _create_downloads_dir_if_not_exists()
+    
+    # Download video
+    download_video(CONST_YOUTUBE_URL)
