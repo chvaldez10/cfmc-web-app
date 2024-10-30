5a656192cca48c4c37755de3a79d7d5d6ceacd74
chvaldez10 <christianvaldez021@gmail.com>
2024-10-29 22:32:34 -0600

    added audio option

--- scripts/youtube_to_mp4.py
+++ scripts/youtube_to_mp4.py
@@ -4,18 +4,16 @@ import os
 
 CONST_YOUTUBE_URL = "https://www.youtube.com/watch?v=sEAeW4BbEnk&ab_channel=BELIEVERSMELODY"
 
-def download_video(url):
+def download_video(download_dir: str):
     try:
         print("Fetching video information...")
-        yt = YouTube(url, on_progress_callback=on_progress)
+        yt = YouTube(CONST_YOUTUBE_URL, on_progress_callback=on_progress)
         
         # Get the highest resolution stream
         print("Finding best quality stream...")
         stream = yt.streams.get_highest_resolution()
-        print(stream)
         
         if stream:
-            # Create downloads directory
             print("\nVideo Details:")
             print(f"Title: {yt.title}")
             print(f"Resolution: {stream.resolution}")
@@ -23,11 +21,38 @@ def download_video(url):
             
             # Download the video
             print("\nDownloading video...")
-            # stream.download()
+            stream.download(output_path=download_dir)
             print("✅ Download completed successfully!")
             return True
         else:
             print("❌ No suitable video stream found.")
+            return False
+            
+    except Exception as e:
+        print(f"❌ Error: {str(e)}")
+        return False
+
+def download_audio(download_dir: str):
+    try:
+        print("Fetching video information...")
+        yt = YouTube(CONST_YOUTUBE_URL, on_progress_callback=on_progress)
+        
+        # Get the highest resolution stream
+        print("Finding best quality stream...")
+        stream = yt.streams.get_audio_only()
+        
+        if stream:
+            print("\nVideo Details:")
+            print(f"Title: {yt.title}")
+            print(f"File size: {stream.filesize / (1024*1024):.1f} MB")
+            
+            # Download the video
+            print("\nDownloading audio...")
+            stream.download(output_path=download_dir)
+            print("✅ Download completed successfully!")
+            return True
+        else:
+            print("❌ No suitable audio stream found.")
             return False
             
     except Exception as e:
@@ -47,7 +72,10 @@ def _create_downloads_dir_if_not_exists():
 # Main
 if __name__ == "__main__":
     # Create download directory if not exists
-    _create_downloads_dir_if_not_exists()
-    
+    download_dir = _create_downloads_dir_if_not_exists()
+
     # Download video
-    download_video(CONST_YOUTUBE_URL)
+    download_video(download_dir)
+
+    # Download audio
+    download_audio(download_dir)
