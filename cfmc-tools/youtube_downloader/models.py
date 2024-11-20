425e91b0c86cadc9403df123d52134c101c69f3d
chvaldez10 <christianvaldez021@gmail.com>
2024-11-19 21:56:19 -0700

    migrate download request

--- cfmc-tools/youtube_downloader/models.py
+++ cfmc-tools/youtube_downloader/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+class DownloadRequest(models.Model):
+    url = models.URLField(max_length=2000)
+    
+    download_type = models.CharField(max_length=10, choices=[
+        ('audio', 'Audio'),
+        ('video', 'Video')
+    ])
+    
+    created_at = models.DateTimeField(auto_now_add=True)
+    status = models.CharField(max_length=20, choices=[
+        ('pending', 'Pending'),
+        ('completed', 'Completed'),
+        ('failed', 'Failed')
+    ])
