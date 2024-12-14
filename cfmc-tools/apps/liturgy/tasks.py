e90cb78f2a539f9038080721f583cdd36e7efff9
chvaldez10 <christianvaldez021@gmail.com>
2024-12-14 09:52:00 -0700

    add tetries in celery worker

--- cfmc-tools/apps/liturgy/tasks.py
+++ cfmc-tools/apps/liturgy/tasks.py
@@ -1,13 +1,18 @@
-from celery import shared_task
-from django.db import transaction
 import logging
+from django.db import transaction
+from celery import shared_task
+from celery.exceptions import SoftTimeLimitExceeded
 
 from .services.liturgy_service import LiturgyService
 from .models import LiturgyEntry
 
 logger = logging.getLogger(__name__)
 
-@shared_task(bind=True)
+@shared_task(bind=True,
+             max_retries=3,
+             soft_time_limit=60,
+             time_limit=300,
+             default_retry_delay=60)
 def parse_liturgy(self, liturgy_entry_id: int):
     """
     Celery task to parse liturgy text
@@ -20,8 +25,15 @@ def parse_liturgy(self, liturgy_entry_id: int):
             entry = LiturgyEntry.objects.get(id=liturgy_entry_id)
             
             # Prevent re processing
-            if entry.status in ['processing', 'failed', 'completed']:
+            if entry.status in ['processing', 'completed']:
                 logger.warning(f"Liturgy entry {liturgy_entry_id} has already been processed")
+                return
+            
+            if entry.status == 'pending' and self.request.retries > self.max_retries:
+                entry.status = 'failed'
+                entry.error_message = f"Liturgy entry {liturgy_entry_id} has failed too many times"
+                entry.save()
+                logger.error(f"Liturgy entry {liturgy_entry_id} has failed too many times")
                 return
             
             # Set status to processing
@@ -40,7 +52,23 @@ def parse_liturgy(self, liturgy_entry_id: int):
     except LiturgyEntry.DoesNotExist:
         logger.error(f"Liturgy entry {liturgy_entry_id} not found")
         return f"Liturgy entry {liturgy_entry_id} not found"
-    
+    except SoftTimeLimitExceeded:
+        entry = LiturgyEntry.objects.get(id=liturgy_entry_id)
+        entry.status = 'failed'
+        entry.error_message = f"Liturgy entry {liturgy_entry_id} took too long to process"
+        entry.save()
+        logger.error(f"Liturgy entry {liturgy_entry_id} took too long to process")
+
+        # retry the task
+        raise self.retry(exc=TimeoutError("Task took too long to process"))
     except Exception as e:
-        logger.error(f"Failed to parse liturgy entry {liturgy_entry_id}: {str(e)}")
-        return f"Failed to parse liturgy entry {liturgy_entry_id}: {str(e)}"
+        try:
+            entry = LiturgyEntry.objects.get(id=liturgy_entry_id)
+            entry.status = 'failed'
+            entry.error_message = str(e)
+            entry.save()
+        except Exception as save_error:
+            logger.error(f"Could not update entry status: {save_error}")
+        
+        # Retry the task with exponential backoff
+        raise self.retry(exc=e)
