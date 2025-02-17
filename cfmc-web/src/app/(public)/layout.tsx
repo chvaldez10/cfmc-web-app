3f3b1fbb283620d1b5acb6cd17f3060c779ad389
chvaldez10 <christianvaldez021@gmail.com>
2025-02-17 11:55:41 -0700

    remove sign up in navbar

--- cfmc-web/src/app/(public)/layout.tsx
+++ cfmc-web/src/app/(public)/layout.tsx
@@ -1,6 +1,12 @@
 import { ReactNode } from "react";
 import { Box } from "@chakra-ui/react";
+import PublicNavbar from "@/components/navigation/PublicNavbar";
 
 export default function PublicLayout({ children }: { children: ReactNode }) {
-  return <Box>{children}</Box>;
+  return (
+    <Box>
+      <PublicNavbar />
+      {children}
+    </Box>
+  );
 }
