8ac2aec547f136761a0a42887643eacf1b4802d7
chvaldez10 <christianvaldez021@gmail.com>
2025-02-18 19:11:53 -0700

    add fade animations

--- cfmc-web/src/app/(public)/_components/HomeJumbotron.tsx
+++ cfmc-web/src/app/(public)/_components/HomeJumbotron.tsx
@@ -1,4 +1,12 @@
-import { Flex, Button, Heading, Text, VStack, Divider } from "@chakra-ui/react";
+import {
+  Flex,
+  Button,
+  Heading,
+  Text,
+  VStack,
+  Divider,
+  SlideFade,
+} from "@chakra-ui/react";
 import { FaArrowRight } from "react-icons/fa";
 import TwoByOneGrid from "@/components/hero/layouts/TwoByOneGrid";
 import { Branding, ModalButtonLabels } from "@/constants/shared/enums";
@@ -17,48 +25,83 @@ export default HomeJumbotron;
 
 const HomeJumboDetails = () => {
   return (
-    <VStack
-      spacing={{ base: 2, md: 4 }}
-      align="start"
-      maxW={{ base: "full", md: "lg" }}
-      mx="auto"
-      borderLeft="solid"
-      borderLeftColor="purple.600"
-      borderLeftWidth={{ base: "0px", md: "4px" }}
-      pl={{ base: 0, md: 4 }}
-      py={4}
+    <SlideFade
+      in={true}
+      offsetX="-50px"
+      offsetY="0px"
+      transition={{ enter: { duration: 0.4 } }}
     >
-      {/* Super script Header*/}
-      <Flex alignItems="center" gap={2}>
-        <Divider
-          orientation="horizontal"
-          borderColor="purple.500"
-          borderWidth="2px"
-          width="12px"
-        />
-        <Text
-          fontSize={{ base: "xs", md: "sm" }}
-          fontWeight="bold"
-          color="gray.600"
-        >
-          {Branding.CHURCH_NAME}
-        </Text>
-      </Flex>
-      <Heading as="h1" size={{ base: "xl", md: "2xl" }} color="purple.400">
-        {MissionStatement.label}
-      </Heading>
-      <Text fontSize={{ base: "sm", md: "md" }}>
-        {MissionStatement.statement}
-      </Text>
-      <Button
-        colorScheme="purple"
-        rightIcon={<FaArrowRight />}
-        variant="solid"
-        size={{ base: "sm", md: "md" }}
+      <VStack
+        spacing={{ base: 2, md: 4 }}
+        align="start"
+        maxW={{ base: "full", md: "lg" }}
+        mx="auto"
+        borderLeft="solid"
+        borderLeftColor="purple.600"
+        borderLeftWidth={{ base: "0px", md: "4px" }}
+        pl={{ base: 0, md: 4 }}
+        py={4}
       >
-        {ModalButtonLabels.ANNOUNCEMENTS}
-      </Button>
-    </VStack>
+        {/* Super script Header*/}
+        <SlideFade
+          in={true}
+          offsetX="0px"
+          offsetY="-50px"
+          transition={{ enter: { duration: 0.4, delay: 0.4 } }}
+        >
+          <Flex alignItems="center" gap={2}>
+            <Divider
+              orientation="horizontal"
+              borderColor="purple.500"
+              borderWidth="2px"
+              width="12px"
+            />
+            <Text
+              fontSize={{ base: "xs", md: "sm" }}
+              fontWeight="bold"
+              color="gray.600"
+            >
+              {Branding.CHURCH_NAME}
+            </Text>
+          </Flex>
+        </SlideFade>
+        <SlideFade
+          in={true}
+          offsetX="0px"
+          offsetY="-50px"
+          transition={{ enter: { duration: 0.4, delay: 0.8 } }}
+        >
+          <Heading as="h1" size={{ base: "xl", md: "2xl" }} color="purple.400">
+            {MissionStatement.label}
+          </Heading>
+        </SlideFade>
+        <SlideFade
+          in={true}
+          offsetX="0px"
+          offsetY="-50px"
+          transition={{ enter: { duration: 0.4, delay: 1.2 } }}
+        >
+          <Text fontSize={{ base: "sm", md: "md" }}>
+            {MissionStatement.statement}
+          </Text>
+        </SlideFade>
+        <SlideFade
+          in={true}
+          offsetX="0px"
+          offsetY="-50px"
+          transition={{ enter: { duration: 0.4, delay: 1.6 } }}
+        >
+          <Button
+            colorScheme="purple"
+            rightIcon={<FaArrowRight />}
+            variant="solid"
+            size={{ base: "sm", md: "md" }}
+          >
+            {ModalButtonLabels.ANNOUNCEMENTS}
+          </Button>
+        </SlideFade>
+      </VStack>
+    </SlideFade>
   );
 };
 
