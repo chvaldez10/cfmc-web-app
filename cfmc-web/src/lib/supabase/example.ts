53cbae9178258fc7b42e878ce60479b7be60bbb3
chvaldez10 <christianvaldez021@gmail.com>
2025-08-03 12:19:07 -0600

    setup supabase

---
+++ cfmc-web/src/lib/supabase/example.ts
@@ -0,0 +1,79 @@
+// "use server";
+
+// import { SundaysAndSpecialDays } from "@/types/supabase/worship";
+// import { getNextSunday } from "../../common/dateUtils";
+// import { createClient } from "@/utils/supabase/server";
+
+// /*
+//   @param data: any - The data to be formatted to the SundaysAndSpecialDays type
+//   @returns SundaysAndSpecialDays - The formatted data
+// */
+// function formatSundaysAndSpecialDaysData(data: any): SundaysAndSpecialDays {
+//   return {
+//     id: data.id,
+//     created: new Date(data.created),
+//     updated: new Date(data.updated),
+//     date: new Date(data.date),
+//     sundayEventName: data.sunday_event_name,
+//     liturgicalColor: data.liturgical_color,
+//     scriptureReadings: data.scripture_readings,
+//     preacher: data.preacher,
+//     sermonTitle: data.sermon_title,
+//     sermonSeries: data.sermon_series,
+//     preacherNotes: data.preacher_notes,
+//     sundayEventAlternateName: data.sunday_event_alternate_name,
+//     eventId: data.event_id,
+//     liturgy: data.liturgy,
+//     announcements: data.announcements,
+//     hymnOfDiscipleship: data.hymn_of_discipleship,
+//   };
+// }
+
+// export async function getSundaysAndSpecialDays(): Promise<SundaysAndSpecialDays | null> {
+//   const supabase = createClient();
+//   const nextSunday = getNextSunday();
+
+//   // Format the date to the format expected by the database UTC
+//   const formattedDate = nextSunday
+//     .toISOString()
+//     .replace("T", " ")
+//     .replace("Z", "+00");
+
+//   const { data, error } = await supabase
+//     .from("sundays_and_special_days")
+//     .select("*")
+//     .eq("date", formattedDate)
+//     .single();
+
+//   if (error) {
+//     console.error(error);
+//     return null;
+//   } else if (!data) {
+//     console.error("No Sundays and Special Days found");
+//     return null;
+//   }
+
+//   return formatSundaysAndSpecialDaysData(data);
+// }
+
+// export async function getSundaysAndSpecialDaysById(
+//   eventId: number
+// ): Promise<SundaysAndSpecialDays | null> {
+//   const supabase = createClient();
+
+//   const { data, error } = await supabase
+//     .from("sundays_and_special_days")
+//     .select("*")
+//     .eq("event_id", eventId)
+//     .single();
+
+//   if (error) {
+//     console.error(error);
+//     return null;
+//   } else if (!data) {
+//     console.error("No Sundays and Special Days found");
+//     return null;
+//   }
+
+//   return formatSundaysAndSpecialDaysData(data);
+// }
