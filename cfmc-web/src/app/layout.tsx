5b4d9170ddf7269ca1e0ebd9382429f282937639
chvaldez10 <christianvaldez021@gmail.com>
2025-08-09 10:20:18 -0600

    add seo

--- cfmc-web/src/app/layout.tsx
+++ cfmc-web/src/app/layout.tsx
@@ -4,7 +4,23 @@ import { fonts } from "./fonts";
 
 export const metadata: Metadata = {
   title: "CFMC",
-  description: "Calgary Filipino Methodist Church",
+  description: "Calgary Filipino Methodist Church official website.",
+  openGraph: {
+    title: "CFMC - Calgary Filipino Methodist Church",
+    description: "Calgary Filipino Methodist Church official website.",
+    url: "https://cfmc-web.vercel.app/",
+    siteName: "CFMC",
+    images: [
+      {
+        url: "https://cfmc-web.vercel.app/gallery/IMG_4523.JPG",
+        width: 1200,
+        height: 630,
+        alt: "CFMC Church",
+      },
+    ],
+    locale: "en_US",
+    type: "website",
+  },
 };
 
 export default function RootLayout({
