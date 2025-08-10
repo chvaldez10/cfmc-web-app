ac7e3c65df5e972ea704909d3153e775584ac9ae
chvaldez10 <christianvaldez021@gmail.com>
2025-08-10 10:13:11 -0600

    delete discover ministries

--- cfmc-web/src/app/(public)/_sections/index.ts
+++ cfmc-web/src/app/(public)/_sections/index.ts
@@ -1,5 +1,4 @@
 export { default as HomeJumbotron } from "./HomeJumbotron";
 export { default as PassedLiveStream } from "./PassedLiveStream";
-export { default as DiscoverMinistries } from "./DiscoverMinistries";
 export { default as VisionMissionParallax } from "./VisionMissionParallax";
 export { default as LayeredParallax } from "./LayeredParallax";
--- cfmc-web/src/app/(public)/page.tsx
+++ cfmc-web/src/app/(public)/page.tsx
@@ -2,7 +2,6 @@
 import {
   HomeJumbotron,
   PassedLiveStream,
-  DiscoverMinistries,
   VisionMissionParallax,
   LayeredParallax,
 } from "./_sections";
@@ -12,6 +11,7 @@ import { SectionHeader } from "@/components/ui/headers";
 
 // Data
 import { mockEvents } from "@/data/mock/mockEvents";
+import { ABOUT_CONTENT } from "@/constants/shared/about";
 
 export default async function Page() {
   return (
@@ -21,11 +21,10 @@ export default async function Page() {
       <VisionMissionParallax />
       <LayeredParallax />
       <SectionHeader
-        title="Our Community"
-        description="The community of Calgary Filipino Methodist Church is a warm, Christ-centered family of believers rooted in Filipino faith and culture. United in love and service, we gather to worship, grow in God's Word, and support one another through life's journeys. Our fellowship is marked by hospitality, prayer, and joyful worship, as we seek to reflect Christ's light in our homes, church, and the wider Calgary community. Together, we are building a legacy of faith for future generations."
+        title={ABOUT_CONTENT.GET_INVOLVED.title}
+        description={ABOUT_CONTENT.GET_INVOLVED.description}
         mt="0"
       />
-      <DiscoverMinistries />
       <EventSwiper events={mockEvents} />
       <FooterInquiries />
     </>
