46ed8d6663186c7acedf0c72032cdce597a15323
chvaldez10 <christianvaldez021@gmail.com>
2025-08-10 11:46:33 -0600

    fix missing import

--- cfmc-web/src/app/(public)/about/beliefs/page.tsx
+++ cfmc-web/src/app/(public)/about/beliefs/page.tsx
@@ -1,13 +1,6 @@
 import { Article } from "@/components/ui/text";
 import whatWeBelieve from "@/constants/whatWeBelieve";
-import { WHAT_WE_BELIEVE_HEADER } from "@/constants/whatWeBelieve";
 
 export default function BeliefsPage() {
-  return (
-    <Article
-      title={WHAT_WE_BELIEVE_HEADER.title}
-      description={WHAT_WE_BELIEVE_HEADER.description}
-      items={whatWeBelieve}
-    />
-  );
+  return <Article items={whatWeBelieve} />;
 }
