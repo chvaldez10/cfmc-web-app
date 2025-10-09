34ccf466d66caac9352af965a2ffeaa5b74740c7
chvaldez10 <christianvaldez021@gmail.com>
2025-10-09 09:10:52 -0600

    add media inquiry page

---
+++ cfmc-web/src/app/(public)/church-media/inquiry/page.tsx
@@ -0,0 +1,13 @@
+import { HeroHeader } from "@/components/hero";
+
+export default function AnnouncementsPage() {
+  return (
+    <>
+      <HeroHeader
+        title="Media"
+        titleHighlight="Inquiry"
+        description="Feel free to inquire about our services."
+      />
+    </>
+  );
+}
